600d96cedc15876fb6209116f59cecec132cf79a
Sabarish Sankaran B <110752601+sabarishh14@users.noreply.github.com>
2024-09-14 12:00:08 +0530

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import "./globals.css"
+
+export const metadata = {
+  title: 'Passcraft',
+  description: 'Lock your deepest secrets with the ultimate password',
+}
+ 
+export default function RootLayout({ children }) {
+ return (
+    <html lang="en">
+      <head>
+        <link href='https://fonts.googleapis.com/css?family=Roboto+Mono' rel='stylesheet'/>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
